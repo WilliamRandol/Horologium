2587aba0a8ae85022b5a855b2156e146f29c676a
WilliamRandol <william.randol@gmail.com>
2020-02-07 14:45:43 -0700

    Update webcomponent

--- scripts/horologium.js
+++ scripts/horologium.js
@@ -2,110 +2,143 @@ class Horologium extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({ mode: 'open' });
-        this._build();
-    }
-
-    _build() {
-        const clock = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        const content = document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject');
-        const clocktext = document.createElement('div');
-        const style = document.createElement('style');
-
-        clock.setAttribute('viewBox', '0 0 1920 1080');
-        content.setAttribute('x', '0');
-        content.setAttribute('y', '12');
-        content.setAttribute('width', '1920');
-        content.setAttribute('height', '1080');
-        clocktext.setAttribute('id', 'clocktext');
-        style.innerText = this._style;
-
-        clock.appendChild(content);
-        content.appendChild(clocktext);
-
-        this.shadowRoot.appendChild(clock);
-        this.shadowRoot.appendChild(style);
+        this.shadowRoot.appendChild(this._template.content.cloneNode(true));
+        this._updateClock();
     }
 
     _updateClock() {
         const d = new Date();
-        const clocktext = this.shadowRoot.querySelector('#clocktext');
-
-        clocktext.innerHTML =
-            `<div>
-                     <p>ANNUS</p>
-                     <p>${this._romanize(d.getFullYear())}</p>
-                 </div>
-                 <div>
-                     <p>MENSIS</p>
-                     <p>${this._romanize(d.getMonth() + 1)}</p>
-                 </div>
-                 <div>
-                     <xhtmlp>DOMUS</p>
-                     <p>${this._romanize(d.getDate())}</p>
-                 </div>
-                 <div>
-                     <p>HORA</p>
-                     <p>${this._romanize(d.getHours())}</p>
-                 </div>
-                 <div>
-                      <p>MINUTUS</p>
-                      <p>${this._romanize(d.getMinutes())}</p>
-                 </div>
-                 <div>
-                     <p>SECUNDUS</p>
-                     <p>${this._romanize(d.getSeconds())}</p>
-                 </div>`;
+        this._dateTimeParts.forEach((dtp) => this._setValue(d, dtp));
         setTimeout(() => this._updateClock(), 1000);
     }
 
+    _setValue(date, dateTimePart){
+        const romanizedValue = this._romanize(this._getDateTimePartValue(date, dateTimePart));
+        if(this.shadowRoot.querySelector('#' + dateTimePart).innerHTML != romanizedValue){
+            this.shadowRoot.querySelector('#' + dateTimePart).innerHTML = romanizedValue;
+        }
+    }
+
+    _getDateTimePartValue(date, dateTimePart){
+        switch (dateTimePart){
+            case 'year':
+                return date.getFullYear();
+            case 'month':
+                return date.getMonth() + 1;
+            case 'day':
+                return date.getDate();
+            case 'hour':
+                return date.getHours();
+            case 'minute':
+                return date.getMinutes();
+            case 'second':
+                return date.getSeconds();
+            default:
+                return 0;
+        }
+    }
+
     _romanize(num) {
-        if (!+num)
-            return '&nbsp;';
-        var digits = String(+num).split(''),
-            key = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM',
-                '', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC',
-                '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'],
-            roman = '',
-            i = 3;
-        while (i--)
-            roman = (key[+digits.pop() + (i * 10)] || '') + roman;
+        if (!num) return '&nbsp;';
+
+        const digits = String(num).split('');
+        const key = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM',
+                     '', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC',
+                     '', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+        
+        let roman = '';
+        let i = 3;
+
+        while (i--) roman = (key[+digits.pop() + (i * 10)] || '') + roman;
+
         return Array(+digits.join('') + 1).join('M') + roman;
     }
 
     get _style() {
         return `
+        <style>
+            @import url("https://use.typekit.net/xce4phg.css");
             :host {
-                margin: 3% auto;
-                width: 80%;
                 display: block;
             }
             #clocktext {
-                box-shadow: 0.03em 0.03em 0.04em rgba(255, 255, 255, .5), -0.03em -0.03em 0.04em rgba(0, 0, 0, .5), inset 0.03em 0.03em 0.04em rgba(255, 255, 255, .5), inset -0.03em -0.03em 0.04em rgba(0, 0, 0, .5);
                 background-color: rgba(255, 255, 255, .3);
+                box-shadow: 3px 3px 4px rgba(255, 255, 255, .5),
+                            -3px -3px 4px rgba(0, 0, 0, .5),
+                            inset 3px 3px 4px rgba(255, 255, 255, .5),
+                            inset -3px -3px 4px rgba(0, 0, 0, .5);
                 border: .05em solid rgba(0, 0, 0, .7);
-                border-radius: .2em;
+                border-radius: 20px;
+                margin: 6px;
+
                 color: rgba(0, 0, 0, .5);
-                text-shadow: 0.02em 0.02em 0.03em rgba(255, 255, 255, .6), 0em 0em 0.02em rgba(0, 0, 0, .6);
+                text-shadow: 2px 2px 3px rgba(255, 255, 255, .6),
+                             0em 0em 2px rgba(0, 0, 0, .6);
+                text-transform: uppercase;
                 text-align: center;
-                vertical-align: middle;
-                font-size: 120px;
-                margin: .03em;
+                font-size: 127px;
+                font-family: trajan-pro-3, serif;
+                font-weight: 400;
+                font-style: normal;
             }
             
             #clocktext div { 
                 display: inline-block;
-                margin: .25em .5em;
+                margin: 23px 50px;
             }
             
             #clocktext div p {
-                margin: auto;
+                margin: 0;
             }
-            `;
+        </style>
+        `;
+    }
+
+    get _template() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+        <svg viewBox="0 0 1920 1080">
+            <foreignObject x="0" y="0" width="1920" height="1080">
+                <div id="clocktext" lang="la">
+                    <div>
+                        <p>anno</p>
+                        <p id="year"></p>
+                    </div>
+                    <div>
+                        <p>mensis</p>
+                        <p id="month"></p>
+                    </div>
+                    <div>
+                        <p>diebus<p></p>
+                        <p id="day"></p>
+                    </xhtmlp></div>
+                    <div>
+                        <div>
+                            <p>hora</p>
+                            <p id="hour"></p>
+                        </div>
+                        <div>
+                            <p>minute</p>
+                            <p id="minute"></p>
+                        </div>
+                    </div>
+                    <div>
+                        <p>secundus</p>
+                        <p id="second"></p>
+                    </div>
+                </div>
+            </foreignObject>
+        </svg>
+        ${this._style}
+        `;
+        return template;
+    }
+
+    get _dateTimeParts(){
+        return ['year', 'month', 'day', 'hour', 'minute', 'second'];
     }
 
     connectedCallback() {
         this._updateClock();
     }
 }
-
-
