45cca0ff1bdcb09418177f6744b16830612ec692
William Randol <william.randol@gmail.com>
2020-02-20 23:48:47 -0700

    try pwa again

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,49 +1,37 @@
-var cacheName = 'horologium-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/styles/base.css',
-  '/styles/main.css',
-  '/styles/info.css',
-  '/styles/privacy.css',
-  '/styles/tabs.css',
-  '/scripts/main.js',
-  '/scripts/Horologium.js',
-  '/scripts/MenuBar.js',
-  '/scripts/AdHolder.js',
-  '/images/background.jpg',
+self.cacheName = 'horologium-pwa';
+
+self.cacheKeys = [
+    '/',
+    '/index.html',
+    '/styles/base.css',
+    '/styles/main.css',
+    '/styles/info.css',
+    '/styles/privacy.css',
+    '/styles/tabs.css',
+    '/scripts/main.js',
+    '/scripts/Horologium.js',
+    '/scripts/MenuBar.js',
+    '/scripts/AdHolder.js',
+    '/images/background.jpg',
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(self.cacheName).then(function (cache) {
+            return cache.addAll(self.cacheKeys);
+        })
+    );
 });
 
-self.addEventListener('activate', event => {
-    // delete any caches that aren't in expectedCaches
-    // which will get rid of static-v1
-    event.waitUntil(
-      caches.keys().then(keys => Promise.all(
-        keys.map(key => {
-          if (!filesToCache.includes(key)) {
-            return caches.delete(key);
-          }
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.open(self.cacheName).then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
         })
-      )).then(() => {
-        console.log('V2 now ready to handle fetches!');
-      })
     );
-  });
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
 });
