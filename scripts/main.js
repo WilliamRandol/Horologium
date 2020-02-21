b11cafea2ba31626bf0115b9262c825ae0486e2e
William Randol <william.randol@gmail.com>
2020-02-20 22:55:44 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,10 @@
+import Horologium from '/scripts/Horologium.js';
+import MenuBar from '/scripts/MenuBar.js';
+import AdHolder from '/scripts/AdHolder.js';
+
 window.customElements.define('wmr-horologium', Horologium);
 window.customElements.define('menu-bar', MenuBar);
 window.customElements.define('ad-holder', AdHolder);
-
 window.onload = () => {
     'use strict';
   
@@ -9,7 +12,4 @@ window.onload = () => {
       navigator.serviceWorker
                .register('./sw.js');
     }
-    import Horologium from '/scripts/Horologium.js';
-    import MenuBar from '/scripts/MenuBar.js';
-    import AdHolder from '/scripts/AdHolder.js';
   }
