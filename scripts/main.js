48ddd9b1ab6cbe7326c9bf28f1a64f04207d6331
William Randol <william.randol@gmail.com>
2020-02-20 23:54:14 -0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,10 @@ import MenuBar from '/scripts/MenuBar.js';
 import AdHolder from '/scripts/AdHolder.js';
 
 window.onload = () => {
-    navigator.serviceWorker.register('/scripts/sw.js');
+    navigator.serviceWorker.register('/scripts/sw.js').then(
+        registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
+        err => console.log('ServiceWorker registration failed: ', err)
+    );
     window.customElements.define('wmr-horologium', Horologium);
     window.customElements.define('menu-bar', MenuBar);
     window.customElements.define('ad-holder', AdHolder);
