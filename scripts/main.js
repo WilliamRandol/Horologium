edf2687837a7190d81fb05bcb35a81d2017bc743
William Randol <william.randol@gmail.com>
2020-02-20 23:55:41 -0700

    move sw

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ import MenuBar from '/scripts/MenuBar.js';
 import AdHolder from '/scripts/AdHolder.js';
 
 window.onload = () => {
-    navigator.serviceWorker.register('/scripts/sw.js').then(
+    navigator.serviceWorker.register('/sw.js').then(
         registration => console.log('ServiceWorker registration successful with scope: ', registration.scope),
         err => console.log('ServiceWorker registration failed: ', err)
     );
